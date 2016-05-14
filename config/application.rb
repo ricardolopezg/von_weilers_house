beb92b69484af039f98b3e7add88f9a25a5fc2d1
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2016-05-14 12:20:21 -0400

    pics

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module VonWeilershouse
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_assets = true
+    config.assets.paths << Rails.root.join( 'vendor', 'assets' )
+    config.assets.precompile << %w( *.gif *.png )
   end
 end
